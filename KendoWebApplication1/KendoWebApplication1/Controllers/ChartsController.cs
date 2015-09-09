9ebaa7c4b5c2ddd2a65890cf3d5d77a484d8a3aa
Matt Millican <mattmillican@marcushotels.com>
2015-09-09 12:45:57 -0500

    Add an example line chart

---
+++ KendoWebApplication1/KendoWebApplication1/Controllers/ChartsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KendoWebApplication1.Models.Charts;
+
+namespace KendoWebApplication1.Controllers
+{
+    public class ChartsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CallsByCountry()
+        {
+            var data = GenerateData()
+                .OrderBy(x => x.Date);
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        private IEnumerable<CallByCountry> GenerateData()
+        {
+            var data = new List<CallByCountry>
+            {
+                new CallByCountry
+                {
+                    Month = 7,
+                    Year = 2015,
+                    Country = "USA",
+                    CallsCount = 5
+                },
+                //new CallByCountry
+                //{
+                //    Month = 8,
+                //    Year = 2015,
+                //    Country = "USA",
+                //    CallsCount = 3
+                //},
+                new CallByCountry
+                {
+                    Month = 8,
+                    Year = 2015,
+                    Country = "UK",
+                    CallsCount = 9
+                },
+                new CallByCountry
+                {
+                    Month = 7,
+                    Year = 2015,
+                    Country = "UK",
+                    CallsCount = 11
+                }
+            };
+
+            return data;
+        }
+    }
+}
---
+++ KendoWebApplication1/KendoWebApplication1/Models/Charts/CallsByCountryViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoWebApplication1.Models.Charts
+{
+    public class CallsByCountryViewModel
+    {
+        
+    }
+
+    public class CallByCountry
+    {
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public string Period { get { return Month + "/" + Year; } }
+
+        public DateTime Date { get { return new DateTime(Year, Month, 1); } }
+        public string Country { get; set; }
+        public int CallsCount { get; set; }
+    }
+}
